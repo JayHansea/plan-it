7994268b72a246316415ca65e65ee27d5f5715c8
Jay Hansea <ezeokafortochukwu@gmail.com>
2020-04-02 11:54:53 +0100

    made the ProjectList more modular

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Jay Hansea</p>
+        <p className="grey-text">2nd April, 12:51am</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;

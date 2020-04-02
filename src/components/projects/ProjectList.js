f00a7f0dc44360f6f5c622eeba3d6c306eb422a7
Jay Hansea <ezeokafortochukwu@gmail.com>
2020-04-02 01:20:54 +0100

    created Navbar and Dashboard

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Jay Hansea</p>
+          <p className="grey-text">2nd April, 12:51am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Jay Hansea</p>
+          <p className="grey-text">2nd April, 12:51am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Jay Hansea</p>
+          <p className="grey-text">2nd April, 12:51am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;

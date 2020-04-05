d793df1d9a463a77bfaa5fb88f672f628d5b0420
Jay Hansea <ezeokafortochukwu@gmail.com>
2020-04-05 19:27:23 +0100

    adding dummy data

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  projects: [
+    { id: "1", title: "helping in hard times", content: "blah blah blah" },
+    { id: "2", title: "natural selection in play", content: "blah blah blah" },
+    { id: "3", title: "egg hunting, the new life", content: "blah blah blah" },
+  ],
+};
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
